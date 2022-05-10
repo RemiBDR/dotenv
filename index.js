b13e79f9ce7b7afc87ce2354a835cfdaa8050719
Mr BDR <mirebordier@gmail.com>
2022-05-10 13:45:03 +0200

    ok

---
+++ index.js
@@ -0,0 +1,4 @@
+import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`)
